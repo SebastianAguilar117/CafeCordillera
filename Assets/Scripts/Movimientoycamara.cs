d682652e99c6d6307b0eaabac0d1e21010d473a8
Joe <sebastian.aguilarmustafa@gmail.com>
2022-08-18 11:36:29 -0500

    Camara y movimiento
    Script

---
+++ Assets/Scripts/Movimientoycamara.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimientoycamara : MonoBehaviour
+{
+    public GameObject cam;
+    public GameObject campos;
+    public float velocidad; 
+    public float velRotacion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.W)){
+            transform.Translate (new Vector3 (0,0,velocidad * Time.deltaTime));
+        }
+        else{
+            if (Input.GetKey(KeyCode.S)){
+                transform.Translate (new Vector3 (0,0,-velocidad * Time.deltaTime));
+            }
+        }
+        if (Input.GetKey (KeyCode.D)){
+            //campos.transform.SetParent(null);
+            transform.Rotate (new Vector3(0,velRotacion * Time.deltaTime,0));
+            //campos.transform.SetParent(transform);
+        }
+        else{
+            if (Input.GetKey(KeyCode.A)){
+            //campos.transform.SetParent(null);
+            transform.Rotate (new Vector3(0,-velRotacion * Time.deltaTime,0));
+            //campos.transform.SetParent(transform);
+            }
+        }
+        cam.transform.position = Vector3.Lerp (cam.transform.position, campos.transform.position,0.1f);
+    }
+}
