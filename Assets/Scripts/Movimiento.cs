ddf68dfb8564d3d9399c05616ee3a68f01294504
Juan José Gómez Piedrahita <juangomezp2001@outlook.com>
2022-08-30 11:32:18 -0500

    BugDeDeslizamiento

--- Assets/Scripts/Movimiento.cs
+++ Assets/Scripts/Movimiento.cs
@@ -6,6 +6,7 @@ public class Movimiento : MonoBehaviour
 {
     public float velocidad;
     public float rotacion;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,5 +18,10 @@ public class Movimiento : MonoBehaviour
     {
         transform.Translate(0, 0, velocidad * Time.deltaTime * Input.GetAxis("Vertical"));
         transform.Rotate(0, rotacion * Time.deltaTime * Input.GetAxis("Horizontal"),0);
+
+        if ((Input.GetAxis("Vertical") * Input.GetAxis("Vertical"))+(Input.GetAxis("Horizontal")* Input.GetAxis("Horizontal"))<0.01f)
+        {
+            rb.velocity = Vector3.zero;
+        }
     }
 }
